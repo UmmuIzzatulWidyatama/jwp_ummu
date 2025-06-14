0bb2280e54c392220d61b91392c42c48a69df62d
UmmuIzzatulWidyatama <ummu.izzatul@widyatama.ac.id>
2025-06-14 14:00:43 +0700

    ada comment

--- to-do-list-app/app/Config/Routes.php
+++ to-do-list-app/app/Config/Routes.php
@@ -8,4 +8,4 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'ToDoList::index'); //untuk akses index
 $routes->post('tasks/toggle/(:num)', 'ToDoList::toggleStatus/$1'); //untuk memproses ceklis task
 $routes->post('tasks/delete/(:num)', 'ToDoList::delete/$1'); //untuk delete task
-$routes->post('tasks/add', 'ToDoList::add');
+$routes->post('tasks/add', 'ToDoList::add'); //untuk add task
--- to-do-list-app/app/Controllers/ToDoList.php
+++ to-do-list-app/app/Controllers/ToDoList.php
@@ -5,6 +5,7 @@ use App\Models\TodoModel;
 
 class ToDoList extends BaseController
 {
+    //Function index digunakan untuk menampilkan list task
     public function index()
     {
         $model = new TodoModel();
@@ -15,6 +16,11 @@ class ToDoList extends BaseController
         return view('home', $data);
     }
 
+    /**
+     * Toggle status tugas berdasarkan ID.
+     * Jika status saat ini "Selesai", ubah jadi "Belum".
+     * Jika status saat ini "Belum", ubah jadi "Selesai".
+    */
     public function toggleStatus($id)
     {
         $model = new \App\Models\TodoModel();
@@ -28,6 +34,11 @@ class ToDoList extends BaseController
         return redirect()->back();
     }
 
+    /**
+     * Menghapus task berdasarkan ID.
+     * Cek dulu apakah task dengan ID tersebut ada,
+     * jika ada maka hapus dari database.
+    */
     public function delete($id)
     {
         $model = new \App\Models\TodoModel();
@@ -39,6 +50,11 @@ class ToDoList extends BaseController
         return redirect()->back();
     }
 
+    /**
+     * Menambahkan task baru ke dalam database.
+     * Ambil input 'title' dari form POST.
+     * Jika input tidak kosong, simpan sebagai task baru dengan status "Belum".
+    */
     public function add()
     {
         $title = $this->request->getPost('title');
